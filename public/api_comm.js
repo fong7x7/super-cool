c5e6893a11a504068c6b0ebd03a11fca47fe4850
Norman Fong <norman.fong@fortemtech.com>
2018-11-10 04:56:25 -0700

    Added API Comm for communicating with server

---
+++ public/api_comm.js
@@ -0,0 +1,54 @@
+class APIComm {
+    constructor() {}
+
+    sendPlayerLogin(name, callback) {
+        $.ajax({
+            url: '/player/login',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                callback(data);
+            },
+            data: JSON.stringify({name: name})
+        });
+    }
+
+    sendPlayerAction(id, move, shoot, equip, callback) {
+        $.ajax({
+            url: '/player/action',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                callback();
+            },
+            data: JSON.stringify({
+                playerId: id,
+                move: move, // { angle: , magnitude: }
+                shoot: shoot, // { angle: , magnitude: }
+                swap: equip // slot index
+            })
+        });
+    }
+
+    getGameState(callback) {
+        $.ajax({
+            url: '/gamestate',
+            type: 'GET',
+            success: function(data) {
+                callback(data);
+            }
+        });
+    }
+
+    getHeartBeat(callback) {
+        $.ajax({
+            url: '/heartbeat',
+            type: 'GET',
+            success: function(data) {
+                callback(data)
+            }
+        });
+    }
+}
