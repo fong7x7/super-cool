fea935a3cd444df0d54a3a80019b41e4606585b6
Norman Fong <norman.fong@fortemtech.com>
2018-11-14 22:43:14 -0700

    Added comments to weapon.

--- server/state/weapon/weapon.js
+++ server/state/weapon/weapon.js
@@ -1,5 +1,12 @@
 const Entity = require("../entity.js");
 
+/**
+ * Weapons are items equipable by the player.
+ * The weapon class is an abstract class that must be extended upon for proper use.
+ * Weapons have an ownerId that can be used to link back to the player that owns it.
+ * Weapons have a fire function that produces an array of projectiles.
+ * @type {Weapon}
+ */
 module.exports = class Weapon extends Entity {
     constructor(name) {
         super();
@@ -21,6 +28,12 @@ module.exports = class Weapon extends Entity {
         }
     }
 
+    /**
+     * Aims the weapon at a given direction.
+     * The values provided will be used to fire the weapon in a direction.
+     * @param angle
+     * @param mag
+     */
     aim(angle, mag) {
         this.aim_angle = angle;
         this.aim_magnitude = mag;
