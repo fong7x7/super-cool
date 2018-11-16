461d0a4f52b95dcda162d40f5e30d1ece677c2ec
Norman Fong <norman.fong@fortemtech.com>
2018-11-15 17:49:48 -0700

    Added test case for hit box creation

--- test/test_collision_math.js
+++ test/test_collision_math.js
@@ -23,6 +23,23 @@ TestCollisionMath = {
         let q2 = { x: 0, y: 5 };
 
         return Assert.True(CollisionMath.intersectsPolygon(poly, q1, q2));
+    },
+
+    testCreateHitBox: function () {
+        let p1 = { x: 0, y: 0, size: 10 };
+
+        let hitBox = CollisionMath.createHitBox(p1);
+
+        if(!Assert.Equal(hitBox[0].x, -5, "First point x is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[0].y,  5, "First point y is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[1].x,  5, "2nd point x is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[1].y,  5, "2nd point y is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[2].x,  5, "3rd point x is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[2].y, -5, "3rd point y is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[3].x, -5, "4th point x is incorrect")) { return false; }
+        if(!Assert.Equal(hitBox[3].y, -5, "4th point y is incorrect")) { return false; }
+
+        return true;
     }
 };
 
